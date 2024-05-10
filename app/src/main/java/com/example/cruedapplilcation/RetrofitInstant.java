284f5d6200a6cfdac965aab4a3edfdd846c0b1bf
mhrah <mhrahatulislam@gmail.com>
2024-05-10 19:05:22 +0600

    commint

---
+++ app/src/main/java/com/example/cruedapplilcation/RetrofitInstant.java
@@ -0,0 +1,19 @@
+package com.example.cruedapplilcation;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstant {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://catalog-writer.ejachai.com/api/v1/shop/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
